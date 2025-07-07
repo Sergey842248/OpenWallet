0e297826d04e8ceecd7558b6dccf90fea021f52e
Sergey842248 <fallahchristine09@gmail.com>
2025-07-07 11:55:48 +0200

    fixed About Symbol not showing in Settings

--- app/src/main/java/nz/eloque/foss_wallet/ui/screens/SettingsScreen.kt
+++ app/src/main/java/nz/eloque/foss_wallet/ui/screens/SettingsScreen.kt
@@ -4,6 +4,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
@@ -11,6 +15,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
 import nz.eloque.foss_wallet.R
+import nz.eloque.foss_wallet.ui.Screen
 import nz.eloque.foss_wallet.ui.screens.BackButton
 import nz.eloque.foss_wallet.ui.view.settings.SettingsView
 import nz.eloque.foss_wallet.ui.view.settings.SettingsViewModel
@@ -27,6 +32,14 @@ fun SettingsScreen(
                 title = { Text(stringResource(R.string.settings)) },
                 navigationIcon = {
                     BackButton(navController)
+                },
+                actions = {
+                    IconButton(onClick = { navController.navigate(Screen.About.route) }) {
+                        Icon(
+                            imageVector = Icons.Default.Info,
+                            contentDescription = stringResource(R.string.about)
+                        )
+                    }
                 }
             )
         }
